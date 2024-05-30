de6c7b30af5ba7a3e9fd53147d061debc39078d9
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-05-30 16:11:55 +0300

    Add files via upload

---
+++ MOD-2_module_2_6.py
@@ -0,0 +1,77 @@
+
+
+# Домашняя работа по уроку "Пространство имен и способы вызова функции"
+
+
+
+
+def single_root_words(root_word, *other_words):
+
+    same_words = []
+
+    for word in other_words:
+
+        if word.lower().count(root_word.lower()) > 0:
+            same_words.append(word)
+
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
+
+
+'''
+______________________________________________________________________________________________________________
+в данной задаче вторым условием выходит пустой список
+предполагаю что позиционный параметр root_word должен быть короче именнованных параметров other_words
+так как является вложенной строкой.
+ниже проводил проверку предположения котрая дала положительный результат.
+ВОПРОС: как в решении задачи должно получиться ['Able', 'Disable']  и почему 'Mable' не вошло в этот список,
+хотя имеет тот же корень  -Able- ?
+______________________________________________________________________________________________________________
+'''
+
+
+def single_root_words(root_word, *other_words):
+
+    same_words = []
+    not_same = []
+    for word in other_words:
+        # Проверяем, содержится ли root_word в слове (с учетом регистра)
+        if word.lower().count(root_word.lower()) > 0:
+            same_words.append(word)
+
+    return same_words
+
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+print(result1)
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result2)
+result3 = single_root_words('Able', 'Disablement', 'Mable', 'Disable', 'Bagel')
+print(result3)
+result4 = single_root_words("Молот", "Молоточек", "Молоток", "Лимон", "Апельсин", "Грейпфрут")
+print(result4)
+result5 = single_root_words("молот", "Молоточек", "молоток", "Лимон", "Апельсин", "Грейпфрут")
+print(result5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
